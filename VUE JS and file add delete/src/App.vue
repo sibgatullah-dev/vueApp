899c1e9d58a05250362f21f3452307a93a04acfb
Sibgatullah <sibgatullah881@gmail.com>
2025-03-17 23:01:29 +0600

    vue api

--- VUE JS and file add delete/src/App.vue
+++ VUE JS and file add delete/src/App.vue
@@ -28,7 +28,10 @@ const removeTask =(index)=>{
 
 onMounted(async() => {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos')//await keyword tells the page to wait untile the data processing is finished
+    //to check api we can use the postman softwear , also there is a built in package for this in vs code
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')// with await keyword the api tells the page to wait untile the data processing is finished
+    const data = await response.json()//Turning the given data from api into json formate
+    task.value = data.map((task) => task.title)// the values of data are pushed inside task and after pushing the title of task was requested 
   } 
   catch (error) {
     console.log('Warning!!');
